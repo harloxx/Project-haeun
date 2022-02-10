018dbbbe804b817f59c6316509707613254692e6
harloxx <chloe0105@naver.com>
2022-02-10 21:54:15 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import requests from './requests';
 import Row from './Row';
 import React from 'react';
-
+import Banner from './Banner';
+import Nav from './Nav';
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build netfilx</h1>
-      <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals}/>
+    <div className="app">
+      <Nav/>
+      <Banner />
+      <Row 
+        title="NETFLIX ORIGINAL" 
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Horror Now" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Now" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries Now" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from './axios';
+import "./Row.css";
+import YouTube from "react-youtube"
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}){
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl,setTrailerUrl]=useState("");
 
     //row 가 로딩될 때마다 렌더링됨
-    useEffect(()=>{
-        async function fetchData(){
-            const request= await axios.get(fetchUrl);
-            console.log(request.data.results);
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
             return request;
         }
@@ -19,21 +22,46 @@ function Row({title, fetchUrl}){
         //만약 [], row가 로딩되면, 한번만 로딩됨
     }, [fetchUrl]);
 
-    return(
+    const opts = {
+        height:"390",
+        width:"100%",
+        playerVars:{
+            autoply:1,
+        }
+    };
+
+    const handleClick = (movie) =>{
+        if(trailerUrl){
+            setTrailerUrl("");
+
+        }else{
+            movieTrailer(movie?.name || "")
+            .then((url)=>{
+                const urlParams=new URLSearchParams(new Url(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            })
+            .catch((error)=> console.log(error));
+        }
+    }
+
+    return (
         <div className="row">
             <h2>{title}</h2>
 
             <div className="row__posters">
-                {movies.map(movie=>{
+                {movies.map((movie) => {
                     return (
-                        <img 
-                            className='row__poster'
-                            src={`${base_url}${movie.poster_path}`} 
+                        <img
+                            key={movie.id}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            src={`${base_url}${
+                                isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                             alt={movie.name}
                         />
                     )
                 })}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
         </div>
     )
 }
